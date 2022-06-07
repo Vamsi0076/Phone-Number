6cbd98e82faa3a28b66a1a864fe2a5ee93b217b9
R.Lohith <96762711+Lohith3699@users.noreply.github.com>
2022-06-07 10:16:50 +0000

    code

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ while True:
     valid_code = True
     break
 if(valid_code==True):
-  while True:
-    num = input('Enter a mobile number to validate: ')
-    Pattern = re.compile("[7-9][0-9]{9}")
-    if Pattern.match(num):
-      print('Valid Mobile Number')
-      break
+ 
+    num = input("Please enter a 10 digit mobile number: ")
+if len(num) > 10 or len(num) < 10:
+    print("Number is not valid (Enter a 10 digit number)")
+else:
+    if num[0] == '6' or num[0] == '7' or num[0] == '8' or num[0] == '9':
+        try:
+            num = int(num)
+            print("Your number is valid")
+        except:
+            print('Number is not valid (A number should not contain any characters)')
     else:
-     print("Invalid Mobile Number")
+        print("Number is not valid (A number should start with 7 or 8 or 9)")
